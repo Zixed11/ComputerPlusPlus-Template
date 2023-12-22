523b1b00f3cd5dd4d753a00026a76c231ffd2346
zixeddev <83886612+Zixed11@users.noreply.github.com>
2023-12-22 14:07:11 -0500

    Add files via upload

---
+++ ComputerPlusPlusTemplate/Plugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using GorillaNetworking;
+using System;
+using UnityEngine;
+using UnityEngine.Device;
+using Utilla;
+using ComputerPlusPlus;
+using WalkSimulator.Tools;
+using ComputerPlusPlus.Tools;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.UI;
+using System.Linq;
+using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using System.IO;
+
+namespace ZixedComputer
+{
+    [BepInPlugin("com.yourname.gorillatag.yourpluginname", "YourPluginName", "1.0.0")]
+    [BepInDependency("com.kylethescientist.gorillatag.computerplusplus")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+    }
+
+    public class ZixedPlusPlus : IScreen
+    {
+        public string Title => "TEMPLATE";
+
+        public string Description => "Press [Option 1] to toggle between this room's";
+
+        bool somethingHappened = false;
+
+        bool RoomInfo;
+
+        public void Start()
+        {
+
+        }
+
+        public string GetContent()
+        {
+            string RoomName = "Room Code: " + PhotonNetwork.CurrentRoom.Name + "\n";
+            string pcount = "Players in this room: " + PhotonNetwork.CurrentRoom.PlayerCount + "\n";
+            string mode = "GameMode: " + GorillaComputer.instance.currentGameMode + "\n\n";
+
+            if (PhotonNetwork.InRoom)
+            {
+                if (RoomInfo)
+                {
+                    return RoomName + pcount + mode;
+                }
+            }
+            else
+            {
+                return "Not connected to room.";
+            }
+
+            return string.Empty;
+        }
+
+        public void OnKeyPressed(GorillaKeyboardButton button)
+        {
+            if (button.characterString == "option1")
+            {
+                RoomInfo = !RoomInfo;
+            }
+        }
+    }
+}
---
+++ ComputerPlusPlusTemplate/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputerPlusPlusTemplate
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.template.gorillatag.computerplusplustemplate";
+        public const string Name = "ComputerPlusPlusTemplate";
+        public const string Version = "1.0.0";
+    }
+}
